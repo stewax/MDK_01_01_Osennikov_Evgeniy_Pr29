f61eb43ddcfc0035bcc519aadc0834d95550bb98
Khodyrev Nikita <sigma29034@gmail.com>
2026-02-19 08:59:55 +0500

    Разработанные модели. Osennikov

---
+++ Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr29.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO {  get; set; }
+        public DateTime RentStart { get; set; }
+        public int Duration { get; set; }
+        public int IdClub { get; set; }
+    }
+}
